f94d82025342e31351ea5db50632c44f9d7b9f7d
Andreea Cojocaru <andra@Andreeas-MacBook-Pro.local>
2019-10-10 23:37:59 +0300

    the actually fixed and working

--- stage2/04-minor-security-hardening/03-run.sh
+++ stage2/04-minor-security-hardening/03-run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -e
 
 install -m 644 files/sshd_config		"${ROOTFS_DIR}/etc/ssh/"
-
+install -d "${ROOTFS_DIR}/etc/banner"
+install -m 644 files/banner "${ROOTFS_DIR}/etc/banner/ssh"
+install -m 644 files/issue.net "${ROOTFS_DIR}/etc/"
 on_chroot << EOF
 echo "${FIRST_USER_NAME} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/010_${FIRST_USER_NAME}-nopasswd
-
+mkdir -p /home/${FIRST_USER_NAME}/.ssh
 EOF
 
+install -m 600 files/authorized_keys "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.ssh"
+
+on_chroot << EOF
+chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}/.ssh
+EOF
